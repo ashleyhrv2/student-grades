2fa5117516c38aea861f7aa3e0015e2dac04ec95
ashleyhrv2 <168749264+ashleyhrv2@users.noreply.github.com>
2025-10-06 12:04:41 -0400

    Add files via upload

---
+++ calculating grades/src/studentgrades/grades.java
@@ -0,0 +1,13 @@
+package studentgrades;
+
+import java.util.Scanner;
+
+public class grades 
+{
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter first course: ");
+	}
+
+}
