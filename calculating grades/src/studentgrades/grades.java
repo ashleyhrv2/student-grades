6f9b0cb45736e7df526a703b29bc684943561f85
ashleyhrv2 <168749264+ashleyhrv2@users.noreply.github.com>
2025-10-07 11:48:15 -0400

    Add files via upload

--- calculating grades/src/studentgrades/grades.java
+++ calculating grades/src/studentgrades/grades.java
@@ -4,10 +4,46 @@ import java.util.Scanner;
 
 public class grades 
 {
+	String course1,class1;
+	static Scanner allinput = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter first course: ");
+		String class1 = allinput.nextLine();
+		
+		
+		System.out.print("How many minutes do you spend on this course a week?");
+		int course1 = input.nextInt();
+		
+		System.out.print("Enter course: ");
+		String class2 = allinput.nextLine();
+		
+		
+		System.out.print("How many minutes do you spend on this course a week?");
+		int course2 = input.nextInt();
+		
+		System.out.print("Enter course: ");
+		String class3 = allinput.nextLine();
+		
+		
+		System.out.print("How many minutes do you spend on this course a week?");
+		int course3 = input.nextInt();
+		
+		System.out.print("Enter course: ");
+		String class4 = allinput.nextLine();
+		
+		
+		System.out.print("How many minutes do you spend on this course a week?");
+		int course4 = input.nextInt();
+		
+		System.out.print("Enter course: ");
+		String class5 = allinput.nextLine();
+		
+		
+		System.out.print("How many minutes do you spend on this course a week?");
+		int course5 = input.nextInt();
+	
 	}
 
 }
